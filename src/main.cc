a05467d8a65362c5da76f3c331bca490757ebca1
Nadeŭka <nadevko@riseup.net>
2024-04-11 11:39:08 +0300

    ui to gresources

--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,21 @@
 #include <gtkmm.h>
+#include <iostream>
 
 Glib::RefPtr<Gtk::Application> app;
 
 int
 main(int argc, char** argv)
 {
+  if (argc > 1 || !gtk_init_check()) {
+    std::cerr << "Program supports only gui mode and zero args\n";
+    return 1;
+  }
+
   app = Gtk::Application::create("io.github.nadevko.cw1");
 
   app->signal_activate().connect([]() {
     auto builder = Gtk::Builder::create();
-    builder->add_from_file("../src/views/about.ui");
+    builder->add_from_resource("/io/github/nadevko/cw1/views/about.ui");
     auto about = builder->get_widget<Gtk::AboutDialog>("about");
     app->add_window(*about);
     about->set_visible(true);
